cb07637e6d9519cf01a3d4a33ae9811d89fee90a
MdAsraful56 <work.mdasraful56@gmail.com>
2025-01-24 13:22:16 +0600

    React router, tailwind, daisy ui install && also tailwind css new version & daisy ui new version so let start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='text-4xl btn'>Vite + React</h1>
+      <button className="btn btn-warning">Warning</button>
+      
+    </>
+  )
+}
+
+export default App
